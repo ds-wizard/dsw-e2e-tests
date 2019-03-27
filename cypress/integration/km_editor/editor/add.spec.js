13c68a0895fc857199f5c939fa769808b21592d1
Jan Slifka <jan.slifka@gmail.com>
2019-03-27 14:19:32 +0100

    Add KM Editor add tag test

--- cypress/integration/km_editor/editor/add.spec.js
+++ cypress/integration/km_editor/editor/add.spec.js
@@ -2,12 +2,13 @@ describe('KM Editor add entity', () => {
     const kmName = 'Test Knowledge Model'
     const kmId = 'test-km'
 
+
     beforeEach(() => {
         cy.task('mongo:delete', {
             collection: 'branches',
             args: { kmId }
         })
-        cy.createKMEditor({kmId, name: kmName, parentPackageId: null})
+        cy.createKMEditor({ kmId, name: kmName, parentPackageId: null })
         cy.loginAs('datasteward')
         cy.visitApp('/km-editor')
     })
@@ -36,5 +37,33 @@ describe('KM Editor add entity', () => {
         cy.get('.input-child a').contains(chapterName).click()
         cy.get('#title').should('have.value', chapterName)
         cy.get('#text').should('have.value', chapterText)
+    })
+
+
+    it('add tag', () => {
+        const tagName = 'Data Management'
+        const tagDescription = 'These questions are about data management.'
+
+        // Open editor
+        cy.clickIndexTableAction(kmId, 'Open Editor')
+        cy.url().should('contain', '/km-editor/edit')
+
+        // Add tag and save
+        cy.get('.link-add-child').contains('Add tag').click()
+        cy.get('#name').clear().type(tagName)
+        cy.get('#description').clear().type(tagDescription)
+        cy.get('.form-group-color-picker a:nth-child(5)').click()
+        cy.get('.btn').contains('Save').click()
+
+        // Open editor again
+        cy.url().should('contain', '/km-editor')
+        cy.clickIndexTableAction(kmId, 'Open Editor')
+        cy.url().should('contain', '/km-editor/edit')
+
+        // Check that the tag is there
+        cy.get('.input-child a').contains(tagName).click()
+        cy.get('#name').should('have.value', tagName)
+        cy.get('#description').should('have.value', tagDescription)
+        cy.get('.form-group-color-picker a:nth-child(5)').should('have.class', 'selected')
     })
 })
