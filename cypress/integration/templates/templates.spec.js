c47a17d911f304392b58266d4124b679ee132c9d
Jan Slifka <jan.slifka@gmail.com>
2021-05-03 10:41:11 +0200

    [DSW-1082] Display usable knowledge models on template detail

--- cypress/integration/templates/templates.spec.js
+++ cypress/integration/templates/templates.spec.js
@@ -1,9 +1,28 @@
 describe('Templates', () => {
+
+    beforeEach(() => {
+        cy.task('mongo:delete', {
+            collection: 'packages',
+        })
+        cy.clearServerCache()
+
+        cy.fixture('test-km-1').then((km) => {
+            cy.importKM(km)
+        })
+    })
+
     it('Default template', () => {
         cy.loginAs('datasteward')
         cy.visitApp('/templates')
+
+        // View template detail
         cy.clickListingItemAction('Questionnaire Report', 'View detail')
         cy.url().should('contain', '/templates/')
         cy.get('h1').contains('Questionnaire Report')
+
+        // Usable knowledge models
+        cy.get('dt').contains('Usable with')
+        cy.get('a').contains('dsw:test-km-1:1.0.0').click()
+        cy.get('.top-header-title').contains('Test Knowledge Model 1')
     })
 })
