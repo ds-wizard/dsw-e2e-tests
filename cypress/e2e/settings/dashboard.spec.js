a71563a6a1ef7d5f95e2192c103bd62058b747f7
Jan Slifka <jan.slifka@gmail.com>
2023-04-17 17:01:25 +0200

    [DSW-1898] Rework welcome warning and info

--- cypress/e2e/settings/dashboard.spec.js
+++ cypress/e2e/settings/dashboard.spec.js
@@ -24,17 +24,68 @@ describe('Settings / Dashboard', () => {
         cy.getCy('dashboard_welcome').should('exist')
     })
 
-    it('welcome info', () => {
-        cy.fillFields({ welcomeInfo: '# Welcome info'})
+    it('login info', () => {
+        cy.fillFields({ loginInfo: '# Welcome\n\nThis is a wizard instance for e2e tests'})
         cy.submitForm()
-        cy.visitApp('/dashboard')
-        cy.getCy('dashboard_alert-info').contains('Welcome info')
+        cy.logout()
+
+        cy.getCy('login_side-info').find('h1').contains('Welcome')
+        cy.getCy('login_side-info').find('p').contains('This is a wizard instance for e2e tests')
     })
 
-    it('welcome warning', () => {
-        cy.fillFields({ welcomeWarning: '# Welcome warning'})
+    it('announcement info', () => {
+        // Add welcome info and fill some details
+        cy.getCy('form-group_list_add-button').click()
+        cy.get('label').contains('Info').click()
+        cy.fillFields({ 'announcements\\.0\\.content': '# Info Announcement'})
+        cy.checkToggle('announcements\\.0\\.dashboard')
         cy.submitForm()
+
+        // It should be visible on the dashboard
         cy.visitApp('/dashboard')
-        cy.getCy('dashboard_alert-warning').contains('Welcome warning')
+        cy.getCy('announcement_info').contains('Info Announcement')
+
+        // But not on the login screen
+        cy.logout()
+        cy.visitApp('/')
+        cy.getCy('announcement_info').should('not.exist')
+    })
+
+    it('announcement warning', () => {
+        // Add welcome info and fill some details
+        cy.getCy('form-group_list_add-button').click()
+        cy.get('label').contains('Warning').click()
+        cy.fillFields({ 'announcements\\.0\\.content': '# Warning Announcement'})
+        cy.checkToggle('announcements\\.0\\.loginScreen')
+        cy.submitForm()
+
+        // It should not be visible on the dashboard
+        cy.visitApp('/dashboard')
+        cy.get('.Dashboard').should('exist')
+        cy.getCy('announcement_warning').should('not.exist')
+
+        // But visible on the login screen
+        cy.logout()
+        cy.visitApp('/')
+        cy.getCy('announcement_warning').contains('Warning Announcement')
+    })
+
+    it('announcement critical', () => {
+        // Add welcome info and fill some details
+        cy.getCy('form-group_list_add-button').click()
+        cy.get('label').contains('Critical').click()
+        cy.fillFields({ 'announcements\\.0\\.content': '# Critical Announcement'})
+        cy.checkToggle('announcements\\.0\\.dashboard')
+        cy.checkToggle('announcements\\.0\\.loginScreen')
+        cy.submitForm()
+
+        // It should be visible on the dashboard
+        cy.visitApp('/dashboard')
+        cy.getCy('announcement_danger').contains('Critical Announcement')
+
+        // And visible on the login screen
+        cy.logout()
+        cy.visitApp('/')
+        cy.getCy('announcement_danger').contains('Critical Announcement')
     })
 })
