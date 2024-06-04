4784e4f7bb0b75f307036f11a8cd2a62e09bbdde
Jan Slifka <jan.slifka@gmail.com>
2024-06-04 09:24:28 +0200

    Fix deprecated KM test

--- cypress/e2e/knowledge-models/deprecated.spec.js
+++ cypress/e2e/knowledge-models/deprecated.spec.js
@@ -10,6 +10,9 @@ describe('Deprecated KM', () => {
     }
 
     before(() => {
+        cy.putDefaultAppConfig()
+
+        cy.task('questionnaire:delete')
         cy.task('package:delete', { km_id: kmId })
         cy.clearServerCache()
 
